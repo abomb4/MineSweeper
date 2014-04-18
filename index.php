690997bd02e40dc0760f9d8f01aaffb17073c9a7
abomb4 <yangrlx@gmail.com>
2014-04-18 18:44:24 +0800

    First commit my version to server.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+UNLIMIT LICENSE
+EVERYONE CAN DO ALL YOU WANT TO DO
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+	<script src="area.js"></script>
+	<script src="point.js"></script>
+	<link rel="stylesheet" type="text/css" href="area.css" />
+        <title>Bonb Sweeper</title>
+    </head>
+    <body>
+	<div id='main'>
+	<script language="javascript">
+		area.write();
+	</script>
+	<?php
+	// put your code heres
+	?>
+	</div>
+    </body>
+</html>
