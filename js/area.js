f564f2141cb956befeb24c947e38bab51e7e1cad
abomb4 <yangrlx@gmail.com>
2019-01-11 18:55:29 +0800

    improve again

--- js/area.js
+++ js/area.js
@@ -6,43 +6,40 @@
 // 棋盘啊我擦
 
 var MineSweeper = (function (element) {
-    // 游戏状态全局变量
-    var ended = false;
-    var started = false;
-    // 初始值
-    var area = {
-        height: 9,
-        width: 9,
-        bombs: 10,
-        board: new Array()
-    };
 
-    var areaElements = new Array();
+    // 游戏状态全局变量
+    var state = {
+        ended: false,
+        started: false
+    };
 
     // 构造函数
     function Area(height, width, bombs) {
         return {
             height: height,
             width: width,
-            bombs: bombs
-            // board: new Array()
+            bombs: bombs,
+            board: new Array()
         };
     }
 
-    function generateID(x, y) {
-        id = x + "_" + y;
-        return id;
+    // 棋盘数据
+    var area = Area(9, 9, 10);
+    // 棋盘点元素
+    var areaElements = new Array();
+
+    function getElement(i, j) {
+        return area.board[i][j].element;
     }
 
-    // useless
-    function test(ev) {
-        ev.preventDefault(true);
-        return false;
+    // Alert something
+    function myAlert(text) {
+        alert(text);
     }
 
     // 打印棋盘
-    area.write = function () {
-        ended = false;
+    function write() {
+        state.ended = false;
         var board = document.createElement('div');
         board.className = 'board';
         element.append(board);
@@ -55,28 +52,14 @@ var MineSweeper = (function (element) {
             area.board[i] = new Array();
             areaElements[i] = new Array();
             for (var j = 0; j < area.width; j++) {
-                area.board[i][j] = newPoint();
-                areaElements[i][j] = document.createElement('div');
-                var point = areaElements[i][j];
-                point.className = 'point';
-                point.onclick = (function (x, y) {
-                    return function () {
-                        pointClick(x, y, true)
-                    };
-                })(i, j);
-                point.oncontextmenu = (function (x, y) {
-                    return function () {
-                        pointFlag(x, y)
-                    };
-                })(i, j);
+                var point = (area.board[i][j] = newPoint(i, j)).element;
 
                 board.append(point);
             }
             board.append(document.createElement('br'));
         }
     };
-
-    area.placeBombs = function (x, y) {
+    function placeBombs(x, y) {
         area.board[x][y].bomb = true;
         var bombs = area.bombs;
         while (bombs--) {
@@ -85,29 +68,43 @@ var MineSweeper = (function (element) {
             do {
                 tempx = parseInt(Math.random() * area.width);
                 tempy = parseInt(Math.random() * area.height);
-                // alert(tempx + " " + tempy);
+                // myAlert(tempx + " " + tempy);
             } while (area.board[tempx][tempy].bomb);
             area.board[tempx][tempy].bomb = true;
         }
         area.board[x][y].bomb = false;
     };
 
-    newPoint = function () {
+    function newPoint(i, j) {
+        var element = document.createElement('div');
+        element.className = 'point';
+        element.onclick = (function (x, y) {
+            return function () {
+                pointClick(x, y, true)
+            };
+        })(i, j);
+        element.oncontextmenu = (function (x, y) {
+            return function () {
+                pointFlag(x, y)
+            };
+        })(i, j);
+
         return {
             bomb: false,
             clicked: false,
             flag: false,
-            surround: 0
+            surround: 0,
+            element: element
         };
     };
 
     function pointClick(x, y) {
         // 结束不可点击
-        if (ended) return;
+        if (state.ended) return;
         // 首次点击放炸弹
-        if (!started) {
-            area.placeBombs(x, y);
-            started = true;
+        if (!state.started) {
+            placeBombs(x, y);
+            state.started = true;
         }
         // 已点过的不响应
         if (area.board[x][y].clicked) {
@@ -118,14 +115,14 @@ var MineSweeper = (function (element) {
             return;
         }
         area.board[x][y].clicked = true;
-        var p = areaElements[x][y];
+        var p = getElement(x, y);
         p.style.cssText = "background-color:#0f0";
         if (area.board[x][y].bomb) {
             p.style.cssText = "background-color:#f00";
             showBombs(false);
-            alert("BOOOOOOOOOOOOOOOOOOOMMMMMMMMMMMMMMM");
-            ended = true;
-            started = false;
+            myAlert("BOOOOOOOOOOOOOOOOOOOMMMMMMMMMMMMMMM");
+            state.ended = true;
+            state.started = false;
         } else {
             checkSurrond(x, y);
             if (area.board[x][y].surround !== 0) {
@@ -133,16 +130,9 @@ var MineSweeper = (function (element) {
                 p.innerHTML = "<p>" + area.board[x][y].surround + "</p>";
             }
         }
-        if (!ended && judge()) {
-            alert("WIN!!!");
+        if (!state.ended && judge()) {
+            myAlert("WIN!!!");
         }
-    }
-
-    function r() {
-        return {
-            x: 0,
-            y: 0
-        };
     }
 
     function checkSurrond(x, y) {
@@ -151,15 +141,19 @@ var MineSweeper = (function (element) {
         var temp = new Array();
         var k = 0;
         for (i = x - 1; i <= x + 1; i++) {
-            if (i < 0 || i >= area.width)
+            if (i < 0 || i >= area.width) {
                 continue;
+            }
             for (j = y - 1; j <= y + 1; j++) {
                 if (j < 0 || j >= area.height)
                     continue;
                 if (area.board[i][j].bomb) {
                     area.board[x][y].surround++;
                 }
-                temp[k] = r();
+                temp[k] = {
+                    x: 0,
+                    y: 0
+                };
                 temp[k].x = i;
                 temp[k++].y = j;
 
@@ -174,14 +168,14 @@ var MineSweeper = (function (element) {
 
     function pointFlag(x, y) {
         // 游戏未开始插旗无效
-        if (!started)
+        if (!state.started)
             return;
         // 不能对已打开的区域插旗
         if (area.board[x][y].clicked)
             return;
 
         area.board[x][y].flag = !area.board[x][y].flag;
-        var p = areaElements[x][y];
+        var p = getElement(x, y);
         if (area.board[x][y].flag) {
             p.innerHTML = "<p>F</p>";
         } else {
@@ -198,8 +192,8 @@ var MineSweeper = (function (element) {
                     return false;
             }
         }
-        ended = true;
-        started = false;
+        state.ended = true;
+        state.started = false;
         showBombs(true);
         return true;
     }
@@ -216,16 +210,19 @@ var MineSweeper = (function (element) {
         for (i = 0; i < area.width; i++) {
             for (j = 0; j < area.height; j++) {
                 if (area.board[i][j].bomb) {
-                    var p = areaElements[i][j];
+                    var p = getElement(i, j);
                     p.style.cssText = color;
                 }
                 if (!area.board[i][j].bomb && area.board[i][j].flag) {
-                    var p = areaElements[i][j];
+                    var p = getElement(i, j);
                     p.innerHTML = "<p>X</p>";
                 }
             }
         }
     }
 
-    return area;
+    return {
+        state: state,
+        write: write
+    };
 });
